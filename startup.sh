537caded1872a579c44503af8ef041d225c401b0
Per Mosegaard <permosegaard@googlemail.com>
2016-05-25 15:32:42 +0100

    Update startup.sh
    minor update

--- startup.sh
+++ startup.sh
@@ -3,12 +3,12 @@
 ip route change default via 172.17.42.254
 if [ -z "${STEAM_USER}" ]; then STEAM_CREDENTIALS="anonymous"; else STEAM_CREDENTIALS="${STEAM_USERNAME} ${STEAM_PASSWORD}"; fi
 
-# start seed update section
-# shell in and rsync /server/* and /root/{Steam,steamcmd}/* to host /seed/$type/{game,steam,steamcmd}
-curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/root/steamcmd/"
-/root/steamcmd/steamcmd.sh +login $STEAM_CREDENTIALS +force_install_dir /server +app_update 740 +quit
-apt-get update && apt-get install -y rsync openssh-client && echo && echo "update complete, pausing..." && read && exit
-# end seed update section
+## start seed update section
+## shell in and rsync /server/* and /root/{Steam,steamcmd}/* to host /seed/$type/{game,steam,steamcmd}
+#curl -s "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -vzx -C "/root/steamcmd/"
+#/root/steamcmd/steamcmd.sh +login $STEAM_CREDENTIALS +force_install_dir /server +app_update 740 +quit
+#apt-get update && apt-get install -y rsync openssh-client && echo && echo "update complete, pausing..." && read && exit
+## end seed update section
 
 if [ "$( find /server/ -type f | wc -l )" -lt "1" ]
 then
