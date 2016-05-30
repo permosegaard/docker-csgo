e36a18cc3b1ae090f50d051d4d946fef32317878
Per Mosegaard <permosegaard@googlemail.com>
2016-05-30 16:10:17 +0100

    Update provision.sh
    minor update

--- provision.sh
+++ provision.sh
@@ -5,8 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Europe/London" > /etc/timezone && dpkg-reconfigure tzdata
 apt-get update && apt-get install -qy iproute2 
 
-apt-get install -qy apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 lib32z1 lib32z1-dev curl
-
-mkdir -p /root/Steam /root/steamcmd /server
+apt-get install -qy aufs-tools apt-utils ca-certificates lib32gcc1 net-tools lib32stdc++6 lib32z1 lib32z1-dev curl
 
 apt-get clean && rm -Rf /var/lib/apt/lists/*
